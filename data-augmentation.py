32d799505d5b8c7a99c940c32516b8edefee0dcb
Ubuntu <ubuntu@ip-172-31-22-211.us-west-2.compute.internal>
2017-05-13 01:28:40 +0000

    use image magik for color convertion

--- data-augmentation.py
+++ data-augmentation.py
@@ -1,10 +1,9 @@
 from tqdm import tqdm
 import os
 import shutil
-from scipy.ndimage import rotate
-from scipy import misc
-import numpy as np
 from constants import ORIGINAL_DATA_DIR, DATA_DIR, ORIGINAL_LABEL_FILE, LABEL_FILE
+
+from subprocess import call
 
 list_imgs = sorted(os.listdir(ORIGINAL_DATA_DIR))
 try:
@@ -17,13 +16,18 @@ os.mkdir(DATA_DIR)
 with tqdm(total=len(list_imgs)) as pbar:
   for f in list_imgs:
     name, ext = f.split(".")
-    im = misc.imread(os.path.join(ORIGINAL_DATA_DIR, f))
+    angle = 0
+    flip = False
     for i in range(8):
-      if i % 4 == 0:
-        im = np.fliplr(im)
-      im = rotate(im, 90)
+      if i == 4:
+        flip = True
+        angle = 0
       f2 = "{n}--{i}.{ext}".format(n=name, i=i, ext=ext)
-      misc.imsave(os.path.join(DATA_DIR, f2), im)
+      if flip:
+        call(["convert", "-rotate", str(angle), "-flip", os.path.join(ORIGINAL_DATA_DIR, f), os.path.join(DATA_DIR, f2)])
+      else:
+        call(["convert", "-rotate", str(angle), os.path.join(ORIGINAL_DATA_DIR, f), os.path.join(DATA_DIR, f2)])
+      angle += 90
     pbar.update(1)
 
 # create new labels
